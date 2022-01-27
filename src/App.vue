5cc939e9bcf0c3e77fc40f1adecdffcf8fec1ad1
GalluFrancesco <latuamaildigithub@email.comfrancesco.gallu@gmail.com>
2022-01-27 16:21:43 +0100

    Add: header component

--- src/App.vue
+++ src/App.vue
@@ -1,27 +1,22 @@
 <template>
   <div id="app">
-   
+    <header-box />
   </div>
 </template>
 
 <script>
+import HeaderBox from './components/HeaderBox.vue'
 
 
 export default {
   name: 'App',
   components: {
+    HeaderBox
     
   }
 }
 </script>
 
 <style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+  @import './style/main.scss'
 </style>
---
+++ src/components/HeaderBox.vue
@@ -0,0 +1,59 @@
+<template>
+    <header>
+        <div class="logo">
+            <img src="../assets/dc-logo.png" alt="">
+        </div>
+        <nav>
+            <ul>
+                <li>CHARACTERS</li>
+                <li>COMICS</li>
+                <li>MOVIES</li>
+                <li>TV</li>
+                <li>GAMES</li>
+                <li>COLLECTABLES</li>
+                <li>VIDEOS</li>
+                <li>FANS</li>
+                <li>NEWS</li>
+                <li>SHOP</li>
+            </ul>
+        </nav>
+    </header>
+</template>
+
+<script>
+
+</script>
+
+<style lang="scss" scoped>
+@import '../style/variables.scss';
+
+    header{
+        display: flex;
+        height: 130px;
+        align-items: center;
+        justify-content: space-between;
+        width: 1000px;
+        margin: 0 auto;
+        
+        .logo{
+            text-align: center;
+        }
+        
+        ul{
+            display: flex;
+
+            li{
+                margin: 0 12px;
+                font-size: 12px;
+                font-weight: bold;
+                line-height: 130px;
+
+                &:hover{
+                    color: $main-blue;
+                    border-bottom: $main-blue solid 4px;
+                }
+            }
+        }
+    }
+
+</style>
