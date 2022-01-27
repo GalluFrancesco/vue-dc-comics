411cce238027e5e877513f85c7660b06ffeb10a0
GalluFrancesco <latuamaildigithub@email.comfrancesco.gallu@gmail.com>
2022-01-27 16:40:51 +0100

    Add: jumbotron component

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,20 @@
 <template>
   <div id="app">
     <header-box />
+    <jumbotron-box />
   </div>
 </template>
 
 <script>
 import HeaderBox from './components/HeaderBox.vue'
+import JumbotronBox from './components/JumbotronBox.vue'
 
 
 export default {
   name: 'App',
   components: {
-    HeaderBox
+    HeaderBox,
+    JumbotronBox
     
   }
 }
---
+++ src/components/JumbotronBox.vue
@@ -0,0 +1,16 @@
+<template>
+    <div class="jumbotron"></div>
+</template>
+
+<script>
+
+</script>
+
+<style lang="scss" scoped>
+    .jumbotron{
+        width: 100%;
+        height: 400px;
+        background: url('../assets/jumbotron.jpg');
+        background-size: cover;
+    }
+</style>
